48b25969284bff48e669be8449afc3fa61cd27cb
Holiday <alega00@mail.ru>
2025-05-27 16:07:14 +0800

    astro nvim config

---
+++ lua/configs/runner.lua
@@ -0,0 +1,11 @@
+require("runner").setup {
+  position = "right", -- position of the terminal window when using the shell_handler
+  -- can be: top, left, right, bottom
+  -- will be overwritten when using the telescope mapping to open horizontally or vertically
+
+  width = 80, -- width of window when position is left or right
+  height = 10, -- height of window when position is top or bottom
+
+  handlers = {}, -- discussed in the next section
+}
+vim.keymap.set("n", "<Leader>rr", require("runner").run)
